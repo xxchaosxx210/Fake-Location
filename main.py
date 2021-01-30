0590f21629c04ebda31508c95cad965ab15a3f5b
xxchaosxx210 <pmillar1279@googlemail.com>
2021-01-30 01:57:12 +0000

    removed listener

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ class MainApp(MDApp):
         super().__init__(**kwargs)
         self.theme_cls.primary_palette = "Blue"
     
-    def on_stop(self):
-        if is_android:
-            self.provider.stop_gps_updates()
-    
     def on_start(self):
         if is_android:
             self.provider = GpsManager(self.on_gps_update)
@@ -34,28 +30,20 @@ class MainApp(MDApp):
     def on_gps_update(self, provider, event, *args):
         """
         any changes to location provider will be sent here
-        """
-        if event == 'location':
-            location = args[0]
-            lat = location.getLatitude()
-            lng = location.getLongitude()
-            if len(self.root.ids["mock_status"].text) > 500:
-                self.root.ids["mock_status"].text = ""
-            self.root.ids["mock_status"].text += f"\n Lat: {lat}, Lng: {lng}"
-        
-        elif event == 'permissions-result':
+        """    
+        if event == 'permissions-result':
             print(args[0])
             print(type(args[0]))
             if args[0] == True:
-                self.provider.start_gps_updates(3000, 10)
+                Logger.info("APP: Location Permission requests have been accepted")
             else:
-                Logger.info("APP: ACCESS_FINE_LOCATION has been rejected")
+                Logger.info("APP: Location Permission requests have been rejected")
     
     def on_get_location(self):
         if is_android:
             loc = self.provider.get_location()
-            print(f"lat = {loc.getLatitude()}, lng = {loc.getLongitude()}")
-            
+            self.root.ids["mock_status"].text += f"\n lat = {loc.getLatitude()}, lng = {loc.getLongitude()}")
+
 
 def main():
     MainApp().run()

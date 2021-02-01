15476feca8e30a3c0a57fab7a2e1f8d50db93cd5
xxchaosxx210 <pmillar1279@googlemail.com>
2021-02-01 15:40:36 +0000

    removed zoom

--- mockmapview.py
+++ mockmapview.py
@@ -17,7 +17,7 @@ class MockMapView(MapView):
         """
         # Check mouse click is in MapView bounds before updating
         if touch.x >= 0.0 and touch.y >= 0.0 and touch.x <= self.width and touch.y <= self.height:
-            lat, lng = self.get_latlon_at(touch.x, touch.y, self.zoom)
+            lat, lng = self.get_latlon_at(touch.x, touch.y, None)
             self.update_target_marker(lat, lng)
         super().on_touch_up(touch)
     
